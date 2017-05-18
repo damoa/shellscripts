b64a136e36d3fb4bb5259a5ece8305ebaea6fc22
Damoa <damoa@gmx.com>
2017-05-18 10:21:08 +0200

    drink.sh

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # shellscripts
+
+## drink.sh
+
+This is a script that reminds you to drink water every hour (until 6pm) after starting it.
---
+++ drink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+H=$(date +%H)
+
+
+while (( 10#$H < 18 )); do
+  sleep 3600
+  spd-say 'drink some water'
+  notify-send "drink water"
+  H=$(date +%H)
+done
+
